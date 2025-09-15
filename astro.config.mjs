a50c9ed4400de1fdf0a921e4f51f20293f1377e6
kamaldhingra1 <dhingra_kk@yahoo.com>
2025-09-15 09:31:36 -0400

    Full Build Docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kamaldhingra1.github.io/profile2",
+  outDir: './docs',
   integrations: [sitemap(), mdx(), pagefind()],
   vite: {
     plugins: [tailwindcss()],
